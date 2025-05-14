5c0657fad85317b996500bbff1c634a7a0861312
Diego Anderson Carvalho <130716128+diegoandersonn@users.noreply.github.com>
2025-05-14 19:35:13 -0300

    fix

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -18,7 +18,6 @@ export default function RootLayout({
   return (
     <AuthProvider>
       <RegisterProvider>
-        <FcmTokenHandler />
         <Toaster />
         {children}
         <ToastContainer
--- src/components/FcmTokenHandler.tsx
+++ src/components/FcmTokenHandler.tsx
@@ -1,11 +1,11 @@
-'use client'
+// 'use client'
 
-import dynamic from 'next/dynamic'
-import useFcmToken from '@/hooks/useFcmToken'
+// import dynamic from 'next/dynamic'
+// import useFcmToken from '@/hooks/useFcmToken'
 
-const FcmTokenHandler = () => {
-  useFcmToken()
-  return null
-}
+// const FcmTokenHandler = () => {
+//   useFcmToken()
+//   return null
+// }
 
-export default dynamic(() => Promise.resolve(FcmTokenHandler), { ssr: false })
+// export default dynamic(() => Promise.resolve(FcmTokenHandler), { ssr: false })
